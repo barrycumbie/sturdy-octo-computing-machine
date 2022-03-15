5b1a7214337eaf8c7e5195e76110dcc0722f29fa
barry-cumbie <barrycumbie@gmail.com>
2022-03-15 18:13:07 -0500

    Create config.js

---
+++ data/config.js
@@ -0,0 +1,26 @@
+// add in card data
+
+// pets: name, type, association, facts {}
+
+// card data
+
+const cardStuff = [
+  { title: "Cumbie Google Scholar Profile",
+    category: "profiles",
+    body: "", 
+    link: "https://scholar.google.com/citations?user=c5Rhw2oAAAAJ&hl=en" 
+  },
+  { title: "Cumbie WikiLabs Profile", 
+    link: "Public Profile:  https://google.qwiklabs.com/public_profiles/5295af94-84cc-4650-ba5b-27aebdaac436"
+  },
+  {
+     title: "Frinkiak",
+     category: "tools", 
+     link: ""
+  },
+  {
+     title: "JustWatch.TV", 
+     category: "tools" 
+  }
+  
+]
